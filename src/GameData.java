e6665b0a09b26c6b7464284ab21481f3d540d3af
Cian2001 <77834603+Cian2001@users.noreply.github.com>
2021-02-12 11:09:55 +0000

    Set number of players armies and neutral armies

--- src/GameData.java
+++ src/GameData.java
@@ -7,6 +7,9 @@ public final class GameData {
 	public static final int NUM_COUNTRIES = 42;
 	public static final int INIT_COUNTRIES_PLAYER = 9;
 	public static final int INIT_COUNTRIES_NEUTRAL = 6;
+	public static final int PLAYERS_NUM_ARMIES = 36;
+	public static final int NEUTRALS_NUM_ARMIES = 24;
+	
 	public static final String[] COUNTRY_NAMES = {
 		"Ontario","Quebec","NW Territory","Alberta","Greenland","E United States","W United States","Central America","Alaska",
 		"Great Britain","W Europe","S Europe","Ukraine","N Europe","Iceland","Scandinavia",
