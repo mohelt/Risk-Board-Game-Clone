3245f3ab321982fa44d5bb62a6d825f9927cb734
Tom Higgins <62621946+tomh00@users.noreply.github.com>
2021-03-29 20:37:48 +0100

    Added types of units represented by each card

--- src/com/whyNotBot/Card.java
+++ src/com/whyNotBot/Card.java
@@ -4,10 +4,23 @@ public class Card {
 	
 	private int countryId;
 	private String countryName;
+	private int unitTypeID; // integer reference to unit type of card
+	private String unitType; // string representation of unit type of card
 
 	Card (int inCountryId, String inCountryName) {   
 		countryId = inCountryId;
 		countryName = inCountryName;
+		unitTypeID = chooseUnitType();
+		switch (unitTypeID){
+			case 0:
+				unitType = "Infantry";
+				break;
+			case 1:
+				unitType = "Cavalry";
+				break;
+			case 2:
+				unitType = "Artillary";
+		}
 		return;
 	}
 	
@@ -17,6 +30,10 @@ public class Card {
 	
 	public String getCountryName () {
 		return countryName;
+	}
+
+	private int chooseUnitType(){
+		return (int)(Math.random() * 3);
 	}
 	
 }
